82cb909305ebe68b5afd5849b7f931ded23d455a
Kamil Wojtol <kamil.wojtol97@gmail.com>
2021-08-03 13:33:05 +0200

    starting film section

--- src/components/Films/Films.js
+++ src/components/Films/Films.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const MOVIE_API =
+  "https://api.themoviedb.org/3/discover/movie?&api_key=fc1a703d1b4bcf6aa6bf88743eca9e34";
 
 export default function Films() {
-  return (
-    <div>
-      <h1>Hi I'm Films component</h1>
-    </div>
-  )
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    axios.get(MOVIE_API).then((res) => setMovies(res));
+    console.log(movies);
+  }, [movies]);
+
+  return <div></div>;
 }
